b22687ad1503c06d529322aa8e3c1cc5dbc56dc4
LizPing <13377139758@163.com>
2026-05-13 13:08:31 +0800

    Add initial configuration files for C++ debugging and building in Visual Studio Code

---
+++ Cpp_Notes/Data-Types/Interger.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *args[]){
+    cout << sizeof(short) << endl;
+    cout << sizeof(int) << endl;
+    cout << sizeof(long) << endl;
+    cout << sizeof(long long) << endl;
+    
+    return 0;
+}
