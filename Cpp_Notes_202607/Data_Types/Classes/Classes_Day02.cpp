cbc3c32d3fc6c200ea55e334b18106a71cc74b0a
LizPing <13377139758@163.com>
2026-07-10 20:26:36 +0800

    Add Notes on Day2

---
+++ Cpp_Notes_202607/Data_Types/Classes/Classes_Day02.cpp
@@ -0,0 +1,36 @@
+/* Day02 */
+// 对象、成员访问、对象指针、对象引用与 this
+#include<iostream>
+
+class Counter{
+private:
+    int value = 0;
+
+public:
+    Counter& increase(){
+        ++value;
+        return *this;
+    }
+
+    Counter& decrease(){
+        --value;
+        return *this;
+    }
+
+    int get_value() {
+        return value;
+    }
+};
+
+int main(){
+    Counter counter;
+    counter
+        .increase()
+        .decrease()
+        .increase()
+        .increase();
+
+    std::cout << counter.get_value() << "\n";
+
+    return 0;
+}
